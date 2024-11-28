8f166c4fbd9c68e53da54ac7671da48d1aba1876
guilhermentos <oguilherme.ramos.soares@gmail.com>
2024-11-28 13:03:02 -0300

    Função de soma

--- hello.py
+++ hello.py
@@ -2,3 +2,10 @@ print("Hello, World!")
 
 a = int(input(print("Digite algum número: ")))
 print(a)
+
+b = int(input(print("Digite outro número: ")))
+print(a)
+
+c = a + b
+
+print("A soma dos números é:",c)
